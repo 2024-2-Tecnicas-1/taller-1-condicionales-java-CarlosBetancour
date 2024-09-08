00ddf618d4b940c12393e6114a31b9333fc89474
CarlosBetancour <159038670+CarlosBetancour@users.noreply.github.com>
2024-09-07 21:16:17 -0500

    Update Triangulo.java
    Se agrego el código de triangulo

--- src/main/java/ejercicios/Triangulo.java
+++ src/main/java/ejercicios/Triangulo.java
@@ -9,8 +9,17 @@ import java.util.Scanner;
  */
 public class Triangulo {
     public static String evaluar(double a, double b, double c) {
-        // TODO: Coloca aquí el código del ejercicio 7: Triángulos
-        return "";
+        if (a <= 0 || b <= 0 || c <= 0 ||
+            a >= b + c || b >= a + c || c >= a + b) {
+            return "No es un triángulo válido";
+        }
+        if (a == b && b == c) {
+            return "El triángulo es equilátero";
+        } else if (a == b || b == c || a == c) {
+            return "El triángulo es isósceles";
+        } else {
+            return "El triángulo es escaleno";
+        }
     }
     
     public static void main(String[] args) {
